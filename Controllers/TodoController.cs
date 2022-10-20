cd2848bec3eb2b86f7880a578d60285d44cd748b
CarlosCamuzzi <carloscamuzzi.hs@gmail.com>
2022-10-20 18:01:48 -0300

    config database

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -2,11 +2,18 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MeuToDo.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MeuToDo.Controllers
 {
-    public class TodoController
-    {
-        
-    }
+  [ApiController]
+  [Route(template: "v1")]
+  public class TodoController : ControllerBase
+  {
+    [HttpGet]
+    [Route(template:"todos")]
+    public List<Todo> Get() => new List<Todo>();
+    
+  }
 }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Configure the HTTP request pipeline.
+if (builder.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
 
+app.MapControllerRoute(
+  name:"default",
+  pattern: "{controller=Home}/{action=Index}/{id?}"
+);
+
+//app.UseHttpsRedirection();
+//app.UseAuthorization();
+//app.MapControllers();
 app.Run();
