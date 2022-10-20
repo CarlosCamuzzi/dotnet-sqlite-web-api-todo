ae1ab2ec763f4a12019e509a1324c27e68f00857
CarlosCamuzzi <carloscamuzzi.hs@gmail.com>
2022-10-20 16:50:29 -0300

    primeiro commit

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MeuToDo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MeuToDo.Data
+{
+  public class AppDbContext : DbContext
+  {
+    public DbSet<Todo> Todos { get; init; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseSqlite(connectionString: "DataSource=app.db;Cache=Shared");
+    }
+  }
+}
